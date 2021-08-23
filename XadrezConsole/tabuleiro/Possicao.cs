4c5417958f74714faec55e38d63e9c7beb813ffd
William Macarini <wpmacarini@hotmail.com>
2021-08-23 10:23:07 -0300

    Classe Posicao

---
+++ XadrezConsole/tabuleiro/Possicao.cs
@@ -0,0 +1,20 @@
+﻿namespace tabuleiro
+{
+    class Possicao
+    {
+        public int Linha{ get; set; }
+        public int Coluna { get; set; }
+
+        public Possicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return Linha
+                + ", "
+                + Coluna;
+        }
+    }
+}
